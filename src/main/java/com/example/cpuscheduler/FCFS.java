477fca150b6471c0ad7f48f1c6a83b2ba45c85f4
larajreige <92322790+larajreige@users.noreply.github.com>
2024-04-15 15:19:51 +0200

    Update FCFS.java

--- src/main/java/com/example/cpuscheduler/FCFS.java
+++ src/main/java/com/example/cpuscheduler/FCFS.java
@@ -1,15 +1,30 @@
 package com.example.cpuscheduler;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class FCFS  extends Scheduler{
+
+    Queue<PCB> queue = new LinkedList<>();
+
 
     @Override
     public void add(PCB newpcb) {
-
+        super.add(newpcb);
+        queue.add(newpcb);
     }
 
 
     @Override
     public void runScheduler() {
-
+        stop = false;
+        PCB currentPcb;
+        while(!stop){
+            if(queue.isEmpty()) continue;
+            currentPcb = queue.poll();
+            executePCB(currentPcb);
+        }
     }
+
+
 }
