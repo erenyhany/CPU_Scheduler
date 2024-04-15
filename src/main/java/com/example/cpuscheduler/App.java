e42f430c17ddd9dbe0f9bb889e2b19ce27dff903
erenyhany <131985785+erenyhany@users.noreply.github.com>
2024-04-15 19:26:47 +0200

    Update App.java

--- src/main/java/com/example/cpuscheduler/App.java
+++ src/main/java/com/example/cpuscheduler/App.java
@@ -12,7 +12,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("CPU_SCHEDULER!");
         stage.setScene(scene);
         stage.show();
